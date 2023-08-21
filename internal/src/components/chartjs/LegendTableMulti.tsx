780143569458e90d734b51e3abe2d399d50e906e
Marvin Poulsen <marpo@lolland.dk>
2023-08-21 12:57:26 +0200

    if number in table - right align

--- internal/src/components/chartjs/LegendTableMulti.tsx
+++ internal/src/components/chartjs/LegendTableMulti.tsx
@@ -1,112 +1,113 @@
 import React from 'react';
 import { useTable } from 'react-table';
-import './legendTable.scss'
-import {toPrettyNumber, getBackgroundColor, getBorderColor} from '../../../utils'
+import './legendTable.scss';
+import { toPrettyNumber, getBackgroundColor, getBorderColor } from '../../../utils';
 
 export interface LegendTableData {
-  name:string;
-  values: number[];
-  on: boolean;
+    name: string;
+    values: number[];
+    on: boolean;
 }
 interface LegendTableProps {
-  colorsStart?: number;
-  headers: string[];
-  data: LegendTableData[];
-  onRowToggle: (rowIndex:number) => void;
-  abbreviation?: string[] | undefined;
+    colorsStart?: number;
+    headers: string[];
+    data: LegendTableData[];
+    onRowToggle: (rowIndex: number) => void;
+    abbreviation?: string[] | undefined;
 }
 
 const createTableData = (data: LegendTableData[], headers: string[], abbreviations?: string[]) => {
-  const tableData: object[] = [];
-  const tableColumns: object[] = [];
-  for (let i = 0; i < headers.length; i++){
-    const header = headers[i];
-    const title = abbreviations && abbreviations[i] ? abbreviations[i] : header;
-    tableColumns.push({ 
-        Header: header,
-        title,
-        accessor: 'col'+(i+1).toString(),
-        Footer: i===0 ? 'I alt': toPrettyNumber(data.filter(item=>item.on).reduce((sum, current) => sum + current.values[i-1], 0) )
-    })
-  }
-data.forEach(element => {
-  const row = {col1: element.name};
-  for (let i = 1; i < headers.length; i++){
-    const accessor = 'col'+(i+1).toString();
-    row[accessor] = toPrettyNumber(element.values[i-1]);
-  }
-  tableData.push(row)
-});
-  return [tableData, tableColumns];
+    const tableData: object[] = [];
+    const tableColumns: object[] = [];
+    for (let i = 0; i < headers.length; i++) {
+        const header = headers[i];
+        const title = abbreviations && abbreviations[i] ? abbreviations[i] : header;
+        tableColumns.push({
+            Header: header,
+            title,
+            accessor: 'col' + (i + 1).toString(),
+            Footer:
+                i === 0
+                    ? 'I alt'
+                    : toPrettyNumber(data.filter((item) => item.on).reduce((sum, current) => sum + current.values[i - 1], 0)),
+        });
+    }
+    data.forEach((element) => {
+        const row = { col1: element.name };
+        for (let i = 1; i < headers.length; i++) {
+            const accessor = 'col' + (i + 1).toString();
+            row[accessor] = toPrettyNumber(element.values[i - 1]);
+        }
+        tableData.push(row);
+    });
+    return [tableData, tableColumns];
 };
 
 function LegendTableMulti(props: LegendTableProps) {
-  const [data, columns] = React.useMemo(
-    () => createTableData(props.data, props.headers, props.abbreviation),
-    [props.data, props.headers, props.abbreviation]
-  );
-  const { getTableProps, getTableBodyProps, headerGroups, footerGroups, rows, prepareRow } =
-    useTable({ columns, data });
-  return (
-    <table
-      {...getTableProps({
-        className:
-          'table is-bordered is-hoverable is-size-7 is-fullwidth is-narrow',
-      })}
-    >
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th {...column.getHeaderProps()}>
-                <abbr title={column.render('title')}>
-                {column.render('Header')}
-                </abbr>
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row);
-          const isOff = !props.data[row.index].on
-          const background = getBackgroundColor(props.colorsStart)
-          const borderColor = getBorderColor(props.colorsStart)
-          return (
-            <tr {...row.getRowProps()} onClick={()=> props.onRowToggle(row.index)}>
-              {row.cells.map((cell) => {
-                return (
-                  <td {...cell.getCellProps({ className: ' content' +(isOff? ' is-off':'') })}>
-                    {cell.column.id === 'col1' && 
-                      <span 
-                        className='color-box' 
-                        style={{
-                          background: background[row.index],
-                          borderColor: borderColor[row.index]
-                        }}
-                      >                        
-                      </span>
-                    }
-                    {cell.render('Cell')}
-                  </td>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </tbody>
-      <tfoot>
-        {footerGroups.map(group => (
-          <tr {...group.getFooterGroupProps()}>
-            {group.headers.map(column => (
-              <th {...column.getFooterProps()}>{column.render('Footer')}</th>
-            ))}
-          </tr>
-        ))}
-      </tfoot>
-    </table>
-  );
+    const [data, columns] = React.useMemo(
+        () => createTableData(props.data, props.headers, props.abbreviation),
+        [props.data, props.headers, props.abbreviation]
+    );
+    const { getTableProps, getTableBodyProps, headerGroups, footerGroups, rows, prepareRow } = useTable({ columns, data });
+    return (
+        <table
+            {...getTableProps({
+                className: 'table is-bordered is-hoverable is-size-7 is-fullwidth is-narrow',
+            })}
+        >
+            <thead>
+                {headerGroups.map((headerGroup) => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map((column) => (
+                            <th {...column.getHeaderProps()}>
+                                <abbr title={column.render('title')}>{column.render('Header')}</abbr>
+                            </th>
+                        ))}
+                    </tr>
+                ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+                {rows.map((row) => {
+                    prepareRow(row);
+                    const isOff = !props.data[row.index].on;
+                    const background = getBackgroundColor(props.colorsStart);
+                    const borderColor = getBorderColor(props.colorsStart);
+                    return (
+                        <tr {...row.getRowProps()} onClick={() => props.onRowToggle(row.index)}>
+                            {row.cells.map((cell) => {
+                                const isNumber = isNaN(cell.value) ? '' : ' has-text-right';
+                                return (
+                                    <td {...cell.getCellProps({ className: ' content' + (isOff ? ' is-off' : '') + isNumber })}>
+                                        {cell.column.id === 'col1' && (
+                                            <span
+                                                className="color-box"
+                                                style={{
+                                                    background: background[row.index],
+                                                    borderColor: borderColor[row.index],
+                                                }}
+                                            ></span>
+                                        )}
+                                        {cell.render('Cell')}
+                                    </td>
+                                );
+                            })}
+                        </tr>
+                    );
+                })}
+            </tbody>
+            <tfoot>
+                {footerGroups.map((group) => (
+                    <tr {...group.getFooterGroupProps()}>
+                        {group.headers.map((column) => (
+                            <th {...column.getFooterProps({ className: isNaN(column.Footer) ? '' : ' has-text-right' })}>
+                                {column.render('Footer')}
+                            </th>
+                        ))}
+                    </tr>
+                ))}
+            </tfoot>
+        </table>
+    );
 }
 
 export default LegendTableMulti;
