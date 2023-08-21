cfafed282240d9bae1418362162ff502f7cb7de4
Marvin Poulsen <marpo@lolland.dk>
2023-08-21 10:45:26 +0200

    possibility of adding title to pie/doughnut

--- internal/src/components/chartjs/PiechartNoLegend.tsx
+++ internal/src/components/chartjs/PiechartNoLegend.tsx
@@ -1,98 +1,99 @@
 import React, { useRef, useEffect } from 'react';
-import {getBackgroundColor, getBorderColor} from '../../../utils'
+import { getBackgroundColor, getBorderColor } from '../../../utils';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend, InteractionMode } from 'chart.js';
 import { Pie, Doughnut } from 'react-chartjs-2';
 
 ChartJS.register(ArcElement, Tooltip, Legend);
-export const options = {
-  responsive: true,
-  interaction: {
-    intersect: true,
-    mode: 'dataset' as InteractionMode,
-  },
-  plugins: {
-    legend: {
-      display: false,
-    },
-    tooltip: {
-      filter: (tooltipItem) => {
-        return tooltipItem.chart.getDataVisibility(tooltipItem.dataIndex)
-      },
-      yAlign:'bottom' as 'bottom',
-      titleAlign: 'center' as 'center',
-      callbacks: {
-        label: (context) => {
-          let totalSum = 0;
-          for (let i = 0;i<context.dataset.data.length;i++){				
-            const on = context.chart.getDataVisibility(i);
-            totalSum += (on ? context.dataset.data[i] : 0);
-          };
-          const label = context.label.length > 15 ? context.label.substring(0,11) + '.. ' : context.label;
-          const tooltipContent = `${label}: ${((context.parsed/totalSum)*100).toFixed(1)}%`;
-          return tooltipContent;
-        }, 
-      }
-    },
-    title: {
-      display: false,
-      text: 'demographySognPie',
-    },
 
-  },
-};
 export interface PiechartData {
-  name: string;
-  value: number;
-  on: boolean;
+    name: string;
+    value: number;
+    on: boolean;
 }
 
 export interface PiechartProps {
-  colorsStart?: number;
-  type?: string;
-  data: PiechartData[];
-  visibility: boolean[];
+    data: PiechartData[];
+    visibility: boolean[];
+    colorsStart?: number;
+    type?: string;
+    title?: string;
 }
 interface PiechartDatasets {
-  data: number[];
-  backgroundColor: string[];
-  borderColor: string[];
-  borderWidth: number;
+    data: number[];
+    backgroundColor: string[];
+    borderColor: string[];
+    borderWidth: number;
 }
 interface PieData {
-  labels: string[];
-  datasets: PiechartDatasets[];
+    labels: string[];
+    datasets: PiechartDatasets[];
 }
 
 export function PiechartNoLegend(props: PiechartProps) {
-  const chartRef = useRef();
-  useEffect(()=>{
-    for (let i=0; i<props.visibility.length;i++){
-      if (chartRef.current.getDataVisibility(i) !== props.visibility[i]){
-        chartRef.current.toggleDataVisibility(i);
-      }
-    }
-    chartRef.current.update();
-  },[props.visibility])
-  const datasets: PiechartDatasets[] = [
-    {
-      data: props.data.map(row=>row.value),
-      backgroundColor: getBackgroundColor(props.colorsStart),
-      borderColor: getBorderColor(props.colorsStart),
-      borderWidth: 1,
-    },
-  ];
-  const data: PieData = {
-    labels: props.data.map(row=>row.name),
-    datasets,
-  };
-  const chartForm = props.type === 'doughnut' ?
-    <Doughnut ref={chartRef} options={options} data={data} /> :
-    <Pie ref={chartRef} options={options} data={data} />;
-  return (
-    <>
-      {chartForm}
-    </>
-  );
+    const chartRef = useRef();
+    useEffect(() => {
+        for (let i = 0; i < props.visibility.length; i++) {
+            if (chartRef.current.getDataVisibility(i) !== props.visibility[i]) {
+                chartRef.current.toggleDataVisibility(i);
+            }
+        }
+        chartRef.current.update();
+    }, [props.visibility]);
+    const options = {
+        responsive: true,
+        interaction: {
+            intersect: true,
+            mode: 'dataset' as InteractionMode,
+        },
+        plugins: {
+            legend: {
+                display: false,
+            },
+            tooltip: {
+                filter: (tooltipItem) => {
+                    return tooltipItem.chart.getDataVisibility(tooltipItem.dataIndex);
+                },
+                yAlign: 'bottom' as 'bottom',
+                titleAlign: 'center' as 'center',
+                callbacks: {
+                    label: (context) => {
+                        // console.log('context: ',context)
+                        let totalSum = 0;
+                        for (let i = 0; i < context.dataset.data.length; i++) {
+                            const on = context.chart.getDataVisibility(i);
+                            totalSum += on ? context.dataset.data[i] : 0;
+                        }
+                        const label = context.label.length > 15 ? context.label.substring(0, 11) + '.. ' : context.label;
+                        const tooltipContent = `${label}: ${((context.parsed / totalSum) * 100).toFixed(1)}%`;
+                        return tooltipContent;
+                    },
+                },
+            },
+            title: {
+                display: props.title ? true : false,
+                text: props.title ? props.title : 'title',
+            },
+        },
+    };
+    const datasets: PiechartDatasets[] = [
+        {
+            data: props.data.map((row) => row.value),
+            backgroundColor: getBackgroundColor(props.colorsStart),
+            borderColor: getBorderColor(props.colorsStart),
+            borderWidth: 1,
+        },
+    ];
+    const data: PieData = {
+        labels: props.data.map((row) => row.name),
+        datasets,
+    };
+    const chartForm =
+        props.type === 'doughnut' ? (
+            <Doughnut ref={chartRef} options={options} data={data} />
+        ) : (
+            <Pie ref={chartRef} options={options} data={data} />
+        );
+    return <>{chartForm}</>;
 }
 
 export default PiechartNoLegend;
