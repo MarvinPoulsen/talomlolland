80afee80181f6af5c3e92c37e2c44169e4c0d123
Marvin Poulsen <marpo@lolland.dk>
2023-09-07 14:45:56 +0200

    numbers align right in tables

--- internal/src/components/summerhouseowner/SummerhouseOwnerTable.tsx
+++ internal/src/components/summerhouseowner/SummerhouseOwnerTable.tsx
@@ -1,72 +1,65 @@
 import React from 'react';
 import { useTable } from 'react-table';
 import { SummerhouseRow } from '../../pages/SummerHouse';
+import { toPrettyNumber } from '../../../utils';
 
 interface SummerhouseResidenceTableProps {
-  data: SummerhouseRow[];
+    data: SummerhouseRow[];
 }
 
 const createTableData = (data) => {
-  const tableData: object[] = [];
-  const tableColumns: object[] = [
-    {
-      Header: 'Region',
-      accessor: 'col1', // accessor is the 'key' in the data
-    },
-    {
-      Header: 'Antal',
-      accessor: 'col2',
-    },
-  ];
-  for (let i = 0; i < data.length; i++) {
-    const tableRow = data[i];
-    tableData.push({ col1: tableRow.region, col2: tableRow.count });
-  }
-  return [tableData, tableColumns];
+    const tableData: object[] = [];
+    const tableColumns: object[] = [
+        {
+            Header: 'Region',
+            accessor: 'col1', // accessor is the 'key' in the data
+        },
+        {
+            Header: 'Antal',
+            accessor: 'col2',
+        },
+    ];
+    for (let i = 0; i < data.length; i++) {
+        const tableRow = data[i];
+        tableData.push({ col1: tableRow.region, col2: toPrettyNumber(tableRow.count) });
+    }
+    return [tableData, tableColumns];
 };
 function SummerhouseResidenceTable(props: SummerhouseResidenceTableProps) {
-  const [data, columns] = React.useMemo(
-    () => createTableData(props.data),
-    [props.data]
-  );
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
-    useTable({ columns, data });
-  return (
-    <table
-      {...getTableProps({
-        className:
-          'table is-bordered is-hoverable is-size-7 is-fullwidth is-narrow',
-      })}
-    >
-      <thead>
-        {headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column) => (
-              <th {...column.getHeaderProps()}>
-                {column.render('Header')}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row) => {
-          prepareRow(row);
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => {
-                return (
-                  <td {...cell.getCellProps({ className: ' content' })}>
-                    {cell.render('Cell')}
-                  </td>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  );
+    const [data, columns] = React.useMemo(() => createTableData(props.data), [props.data]);
+    const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({ columns, data });
+    return (
+        <table
+            {...getTableProps({
+                className: 'table is-bordered is-hoverable is-size-7 is-fullwidth is-narrow',
+            })}
+        >
+            <thead>
+                {headerGroups.map((headerGroup) => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map((column) => (
+                            <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                        ))}
+                    </tr>
+                ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+                {rows.map((row) => {
+                    prepareRow(row);
+                    return (
+                        <tr {...row.getRowProps()}>
+                            {row.cells.map((cell) => {
+                                const isNumber = isNaN(cell.value) ? '' : ' has-text-right';
+                                return (
+                                    <td {...cell.getCellProps({ className: ' content' + isNumber })}>{cell.render('Cell')}</td>
+                                );
+                            })}
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
+    );
 }
 
 export default SummerhouseResidenceTable;
